45c99b29f6cc2d45ed14673f7edb39d652dbc5e2
abdulreh489 <164838072+abdulreh489@users.noreply.github.com>
2025-08-28 19:02:44 +0500

    Update test_py.py

--- test_py.py
+++ test_py.py
@@ -1,22 +1,19 @@
 import pytest
-from app import app   
+from app import app
 
 
 @pytest.fixture
 def client():
-    app.testing = True
     with app.test_client() as client:
         yield client
 
 
-def test_index_route(client):
+def test_index(client):
     response = client.get('/')
     assert response.status_code == 200
-    assert b'Hello, Flask!' in response.data
+    assert b"Hello, Flask!" in response.data
 
 
-def test_about_route(client):
+def test_about(client):
     response = client.get('/about')
     assert response.status_code == 200
-    # Check that some keyword from about.html exists
-    assert b'About' in response.data
